83cc5b75eabb8ef65832f7599cdab8ba6dd652e6
ztepe <zjtepe@gmail.com>
2021-10-22 23:38:59 -0400

    started java form

---
+++ main.js
@@ -0,0 +1,15 @@
+/*jslint browser:true*/
+"use strict";
+
+function validateForm() {
+    var status = false;
+    var x;
+
+    //test for full name
+    x = document.forms.myForm.name.value;
+    console.log(x);
+
+
+
+    return status;
+}
